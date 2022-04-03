efa891a089626db57ba357622a03acdef957bca7
ehsanmavaei <ehsanmavaei@gmail.com>
2022-04-03 22:19:13 +0430

    finish the mobile view

--- app/js/script.js
+++ app/js/script.js
@@ -3,3 +3,22 @@ console.log('HELLO');
 const test = () => {
 	console.log('this is a test');
 };
+ const faqs = document.querySelectorAll(".faq");
+
+
+ faqs.forEach(faq=>{
+	 faq.addEventListener("click",() =>{
+		faq.children[0].children[1].classList.toggle('svg')
+	faq.children[0].children[0].classList.toggle('bold')
+	
+		 faq.classList.toggle('active')
+	 })
+ })
+
+//  const svgs = document.querySelectorAll(".svg");
+
+//  faqs.forEach(svg=>{
+// 	 svg.addEventListener("click",() =>{
+// 		 svg.classList.toggle('svg')
+// 	 })
+//  })
