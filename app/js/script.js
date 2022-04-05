94d213b0c2c7becec45ba343c0dc6ddf3c101c03
ehsanmavaei <ehsanmavaei@gmail.com>
2022-04-05 14:21:36 +0430

    finish the project

--- app/js/script.js
+++ app/js/script.js
@@ -3,22 +3,54 @@ console.log('HELLO');
 const test = () => {
 	console.log('this is a test');
 };
- const faqs = document.querySelectorAll(".faq");
+
+//Order of code steps
+//1. Grab our items
+const items = [...document.querySelectorAll('li.faq__faq-item')]
+//2. Add event listeners to our items that handle an item click.
+//3. handleItemClick function -> 
+//3.1 Grab the clicked item
+//3.2 Grab the itemDetail as we need to add maxHeight
+//3.3 Check if item has .active class
+//3.4 If it does, remove the maxHeight and remove the .active class
+//3.5 If it doesn't, add the maxHeight and add the .active class
+
+const clearItem = (itemDetail) => {
+    itemDetail.style.maxHeight = null
+}
+
+const activateItem = (itemDetail) => {
+    const scrollHeight = itemDetail.scrollHeight
+    itemDetail.style.maxHeight = `${scrollHeight}px`
+}
+
+const handleItemClick = (e) => {
+    const clickedItem = e.target.closest('li')
+    const itemDetail = clickedItem.querySelector('.faq-item__detail')
+    const clickedItemActive = clickedItem.classList.contains('active')
+    clickedItem.classList.toggle('active')
+    if (clickedItemActive) {
+        clearItem(itemDetail)
+    } else {
+        activateItem(itemDetail)
+    }
+};
+
+const addListenersToItems = (array, callback) => {
+    array.forEach(item => {
+        item.addEventListener('click', (e) => {
+            callback(e)
+        })
+    })
+};
 
 
- faqs.forEach(faq=>{
-	 faq.addEventListener("click",() =>{
-		faq.children[0].children[1].classList.toggle('svg')
-	faq.children[0].children[0].classList.toggle('bold')
-	
-		 faq.classList.toggle('active')
-	 })
- })
+addListenersToItems(items, handleItemClick)
 
-//  const svgs = document.querySelectorAll(".svg");
+items[0].click()
 
-//  faqs.forEach(svg=>{
-// 	 svg.addEventListener("click",() =>{
-// 		 svg.classList.toggle('svg')
-// 	 })
-//  })
+
+
+
+
+
